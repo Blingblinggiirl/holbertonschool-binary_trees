9481d1881da6c1d4c968d1f1293a93c746476b3b
ayamorcito <izquierdoz.g@gmail.com>
2022-08-31 14:54:29 -0700

    1 fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -27,6 +27,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
                 parent->left->parent = newnode;
                 newnode->right = NULL;
                 newnode->n = value;
+		return (newnode);
         }
 
         parent->left = newnode;
