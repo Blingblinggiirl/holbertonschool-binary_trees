175d32875aafba095c7702c595f18e9cfc3c5c20
ayamorcito <izquierdoz.g@gmail.com>
2022-09-04 13:00:38 -0700

    14 fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -25,20 +25,17 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        int Lheight = 0;
-        int Rheight = 0;
+	int Lheight = 0;
+	int Rheight = 0;
 
-        if (tree == NULL)
-                return (0);
-
-        if (tree->left != NULL)
-                Lheight = 1 + binary_tree_height(tree->left);
-
-        if (tree->right != NULL)
-                Rheight = 1 + binary_tree_height(tree->right);
-
-        if (Lheight > Rheight)
-                return (Lheight);
-        else
-                return (Rheight);
+	if (tree == NULL)
+		return (0);
+	
+	Lheight = 1 + binary_tree_height(tree->left);
+	Rheight = 1 + binary_tree_height(tree->right);
+	
+	if (Lheight > Rheight)
+		return (Lheight);
+	else
+		return (Rheight);
 }
