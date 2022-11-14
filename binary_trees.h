613aa3d81ddda3f2cd026d759350cf9a095ab084
Blingblinggiirl <Blingblinggiirl@romibenitezz87@gmail.com>
2022-11-13 16:35:35 -0800

    .

--- binary_trees.h
+++ binary_trees.h
@@ -54,4 +54,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 int _pow(int x, int y);
+int binary_tree_is_full(const binary_tree_t *tree);
+int rec_is_full(int is_full, const binary_tree_t *tree);
 #endif
